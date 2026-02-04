918cdc7b152b99a18b9010eac5dcf0448311004e
peng122-byte <870524302@qq.com>
2026-02-04 21:23:39 +0800

    26/02/04-v3

---
+++ 新模板手敲/Driver/LED.c
@@ -0,0 +1,13 @@
+#include "LED.H"
+
+idata unsigned char temp_1 = 0x00;
+idata unsigned char temp_1_old = 0xff;
+//ucLed是且只能是0或1
+
+
+
+
+
+
+
+
---
+++ 新模板手敲/User/main.c
@@ -0,0 +1,5 @@
+#include <STC15F2K60S2.H>
+void main()
+{
+	while(1);
+}
