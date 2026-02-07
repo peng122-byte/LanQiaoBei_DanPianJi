564474e4b7a9a6fdc210a9459d5944970e3851ee
peng122-byte <870524302@qq.com>
2026-02-07 23:43:41 +0800

    26/02/07-v3

--- 新模板手敲/Driver/onewire.h
+++ 新模板手敲/Driver/onewire.h
@@ -1,7 +1,10 @@
 #ifndef _ONEWIRE_H_
 #define _ONEWIRE_H_
 #include <STC15F2K60S2.H>
-
+void Delay_OneWire(unsigned int t);
+void Write_DS18B20(unsigned char dat);
+unsigned char Read_DS18B20(void);
+bit init_ds18b20(void);
 
 
 
